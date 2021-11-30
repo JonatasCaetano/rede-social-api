546cbadb8315e66bf308ac9f67f54acc53eecc4a
Jonatas Caetano <jonatas.calves@gmail.com>
2021-11-30 16:20:34 -0300

    FollowerService class created

---
+++ social-network-api/src/main/java/com/jonatas/socialnetworkapi/services/FollowerService.java
@@ -0,0 +1,13 @@
+package com.jonatas.socialnetworkapi.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jonatas.socialnetworkapi.repositories.FollowerRepository;
+
+@Service
+public class FollowerService {
+
+	@Autowired
+	private FollowerRepository followerRepository;
+}
