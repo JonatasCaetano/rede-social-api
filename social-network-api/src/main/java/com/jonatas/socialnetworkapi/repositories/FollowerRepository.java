3d8eae669a0c035ed2d50e692720ec262849253c
Jonatas Caetano <jonatas.calves@gmail.com>
2021-11-30 16:18:50 -0300

    FollowerRepository interface created

---
+++ social-network-api/src/main/java/com/jonatas/socialnetworkapi/repositories/FollowerRepository.java
@@ -0,0 +1,11 @@
+package com.jonatas.socialnetworkapi.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jonatas.socialnetworkapi.entities.Follower;
+
+@Repository
+public interface FollowerRepository extends MongoRepository<Follower, String>{
+
+}
