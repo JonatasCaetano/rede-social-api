6b1cce1c0b25eb88bfee089b011e777ff46890a4
Jonatas Caetano <jonatas.calves@gmail.com>
2021-12-04 14:45:43 -0300

    EpisodeController class created

---
+++ social-network-api/src/main/java/com/jonatas/socialnetworkapi/controllers/EpisodeController.java
@@ -0,0 +1,15 @@
+package com.jonatas.socialnetworkapi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jonatas.socialnetworkapi.services.EpisodeService;
+
+@RestController
+@RequestMapping(value = "/episodes")
+public class EpisodeController {
+
+	@Autowired
+	private EpisodeService episodeService;
+}
--- social-network-api/src/main/java/com/jonatas/socialnetworkapi/services/EpisodeService.java
+++ social-network-api/src/main/java/com/jonatas/socialnetworkapi/services/EpisodeService.java
@@ -1,5 +1,6 @@
 package com.jonatas.socialnetworkapi.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jonatas.socialnetworkapi.repositories.EpisodeRepository;
@@ -7,5 +8,6 @@ import com.jonatas.socialnetworkapi.repositories.EpisodeRepository;
 @Service
 public class EpisodeService {
 
+	@Autowired
 	private EpisodeRepository episodeRepository;
 }
