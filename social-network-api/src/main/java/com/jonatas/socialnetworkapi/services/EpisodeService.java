3ee7cdc2f81ab31f64c5068838f4b34b2e33700a
Jonatas Caetano <jonatas.calves@gmail.com>
2021-12-04 14:43:24 -0300

    EpisodeService class created

---
+++ social-network-api/src/main/java/com/jonatas/socialnetworkapi/services/EpisodeService.java
@@ -0,0 +1,11 @@
+package com.jonatas.socialnetworkapi.services;
+
+import org.springframework.stereotype.Service;
+
+import com.jonatas.socialnetworkapi.repositories.EpisodeRepository;
+
+@Service
+public class EpisodeService {
+
+	private EpisodeRepository episodeRepository;
+}
