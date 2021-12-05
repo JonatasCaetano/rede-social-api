4a4583a4c506fb404b357818f4106c8d34e7c133
Jonatas Caetano <jonatas.calves@gmail.com>
2021-12-05 16:20:16 -0300

    EvaluationService class created

---
+++ social-network-api/src/main/java/com/jonatas/socialnetworkapi/services/EvaluationService.java
@@ -0,0 +1,11 @@
+package com.jonatas.socialnetworkapi.services;
+
+import org.springframework.stereotype.Service;
+
+import com.jonatas.socialnetworkapi.repositories.EvaluationRepository;
+
+@Service
+public class EvaluationService {
+
+	private EvaluationRepository evaluationRepository;
+}
