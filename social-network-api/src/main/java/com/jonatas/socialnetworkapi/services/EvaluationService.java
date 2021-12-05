aafb4f3c6a27959c7595255002d34e6bc4fd1ed5
Jonatas Caetano <jonatas.calves@gmail.com>
2021-12-05 16:22:17 -0300

    EvaluationController class created

--- social-network-api/src/main/java/com/jonatas/socialnetworkapi/services/EvaluationService.java
+++ social-network-api/src/main/java/com/jonatas/socialnetworkapi/services/EvaluationService.java
@@ -1,5 +1,6 @@
 package com.jonatas.socialnetworkapi.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jonatas.socialnetworkapi.repositories.EvaluationRepository;
@@ -7,5 +8,6 @@ import com.jonatas.socialnetworkapi.repositories.EvaluationRepository;
 @Service
 public class EvaluationService {
 
+	@Autowired
 	private EvaluationRepository evaluationRepository;
 }
