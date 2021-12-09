3636829be783a5c74ed3e382ebe51570941bca3c
Jonatas Caetano <jonatas.calves@gmail.com>
2021-12-09 14:10:40 -0300

    FindAll method implemented

--- social-network-api/src/main/java/com/jonatas/socialnetworkapi/controllers/EntitySaveController.java
+++ social-network-api/src/main/java/com/jonatas/socialnetworkapi/controllers/EntitySaveController.java
@@ -1,6 +1,7 @@
 package com.jonatas.socialnetworkapi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,8 +23,8 @@ public class EntitySaveController {
 	//get
 	
 	@GetMapping(value = "get/all")
-	public String findAll() {
-		return "get all";
+	public ResponseEntity<Object> findAll(){
+		return entitySaveService.findAll();
 	}
 		
 	@GetMapping(value = "get/id/{id}")
--- social-network-api/src/main/java/com/jonatas/socialnetworkapi/services/EntitySaveService.java
+++ social-network-api/src/main/java/com/jonatas/socialnetworkapi/services/EntitySaveService.java
@@ -1,13 +1,31 @@
 package com.jonatas.socialnetworkapi.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.jonatas.socialnetworkapi.entities.EntitySave;
 import com.jonatas.socialnetworkapi.repositories.EntitySaveRepository;
 
 @Service
 public class EntitySaveService {
 
+	//repositories
+	
 	@Autowired
 	private EntitySaveRepository entitySaveRepository;
+
+	//methods
+	
+	public ResponseEntity<Object> findAll(){
+		try {
+			List<EntitySave> list = entitySaveRepository.findAll();
+			return ResponseEntity.ok(list);
+		}catch (RuntimeException e) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+	
 }
